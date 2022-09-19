1730b5d0259f3559e29006bd362ef1b66d2813b6
hbentz <hbentz@ualberta.ca>
2022-09-18 19:28:44 -0700

    some changes to filter based on hamming distance

--- game/elo_vectorized.py
+++ game/elo_vectorized.py
@@ -1,4 +1,3 @@
-from array import array
 import math
 import os
 import time
@@ -6,12 +5,13 @@ import numpy as np
 import itertools
 import game_vectorized
 
-N_CARDS = 5
+MAX_CARDS = 13
 DISPLAY_STRATEGIES = 10
 MAX_ELO = 5000
 MIN_ELO = 1
 N_ROUNDS = 1000
 ELO_K = 32
+HAMMING_FILTER = 6
 
 
 def elo_prob(rating_1, rating_2):
@@ -27,32 +27,33 @@ def normalize_elos(elos):
         return_elos /= np.max(return_elos) / MAX_ELO
     return return_elos
 
+np.set_printoptions(formatter={'float': '{: 0.0f}'.format})
 
-game = game_vectorized.Game(N_CARDS)
-game.compute_strategy_outcomes()
-game.compute_scores()
-game.compute_summary_scores()
+for i in range(3, MAX_CARDS+1):
+    game = game_vectorized.Game(i, hamming_filter=HAMMING_FILTER)
+    game.compute_strategy_outcomes()
+    game.compute_scores()
+    game.compute_summary_scores()
 
-strategy_participation = np.ones(game.strategy_space)/game.strategy_space
-previous_elos = np.ones(game.strategy_space) * 1600
-current_elos = np.ones(game.strategy_space) * 1600
+    strategy_participation = np.ones(game.n_strategies)/game.n_strategies
+    previous_elos = np.ones(game.n_strategies) * 1600
+    current_elos = np.ones(game.n_strategies) * 1600
 
-for n in range(N_ROUNDS):
-    start_time = time.time()
-    for s1, s2 in itertools.combinations(list(range(N_CARDS)), 2):
-        Pa = elo_prob(previous_elos[s1], previous_elos[s2])
-        Pb = 1 - Pa
-        current_elos[s1] += ELO_K * (game.vs_summary[s1, s2] - Pa)# * strategy_participation[s2]
-        current_elos[s2] += ELO_K * (game.vs_summary[s2, s1] - Pb)# * strategy_participation[s1]
-    current_elos = normalize_elos(current_elos)
-    strategy_participation = current_elos / np.sum(current_elos)
-    previous_elos = current_elos.copy()
-    os.system('cls')
+    for n in range(N_ROUNDS):
+        start_time = time.time()
+        for s1, s2 in itertools.combinations(list(range(i)), 2):
+            Pa = elo_prob(previous_elos[s1], previous_elos[s2])
+            Pb = 1 - Pa
+            current_elos[s1] += ELO_K * (game.vs_summary[s1, s2] - Pa)# * strategy_participation[s2]
+            current_elos[s2] += ELO_K * (game.vs_summary[s2, s1] - Pb)# * strategy_participation[s1]
+        current_elos = normalize_elos(current_elos)
+        strategy_participation = current_elos / np.sum(current_elos)
+        previous_elos = current_elos.copy()
+
     sorted_ind = np.argsort(current_elos)
-    print(f"Round {n}:" + ("( %s seconds )" % (time.time() - start_time)))
+    print(f"ELO summary for {i} cards and {N_ROUNDS} iterations.")
     for index in reversed(sorted_ind):
-        if index > DISPLAY_STRATEGIES and index < (game.strategy_space - DISPLAY_STRATEGIES):
+        if index > game.n_strategies and index < (game.n_strategies - DISPLAY_STRATEGIES):
             continue
-        print(f"{game.dealer_space[index]} : {current_elos[index]:0.1f} : {strategy_participation[index]:0.3f}")# : {game.vs_summary[index, list(reversed(sorted_ind))]}")
-
+        print(f"{game.dealer_space[game.strategies[index]]} : {current_elos[index]:0.1f}")# : {game.vs_summary[index, list(reversed(sorted_ind))]}")
 
--- game/game_vectorized.py
+++ game/game_vectorized.py
@@ -3,36 +3,43 @@ import math
 import itertools
 
 class Game:
-    def __init__(self, n_cards):
+    def __init__(self, n_cards, hamming_filter=0):
         self.n_cards = n_cards
         # How many possible deals/pure strategies there are
-        self.strategy_space = math.factorial(n_cards)
+        self.n_deals = math.factorial(n_cards)
 
         # All possible shuffles of an n_card deck are stored in self.dealer_space
         # So that the card dealt on turn n is self.dealer_space[deal_id, n] + 1
         # This doubles as all the possible pure strategies
         # Where a strategy with id n will bid self.dealer_space[n, card - 1]
-        self.dealer_space = np.ones((self.strategy_space, n_cards), dtype=np.int8)
+        self.dealer_space = np.ones((self.n_deals, n_cards), dtype=np.int8)
         
         # Fill the dealerspace with all possible permutations
         for row, permutation in enumerate(itertools.permutations(list(range(n_cards)), n_cards)):
             self.dealer_space[row, :] = permutation
         
+        if hamming_filter > 0:
+            self.strategies = np.where(self.compute_hamming_distances() <= hamming_filter)[0]
+        else:
+            self.strategies = np.arange(self.n_deals)
+
+        self.n_strategies = self.strategies.shape[0]
+
         # self.strategy_outcomes[strategy_1, strategy_2, card] = 1: strategy_1 wins the trick, 0: draw, -1: strategy_2 wins the trick
-        self.strategy_outcomes = np.zeros((self.strategy_space, self.strategy_space, n_cards), dtype=np.int8)
+        self.strategy_outcomes = np.zeros((self.n_strategies, self.n_strategies, n_cards), dtype=np.int8)
         
         # self.scores[strategy_1, strategy_2, deal_id, turn] = the score advantage strategy1 gained from turn on deal_id
-        self.scores = np.zeros((self.strategy_space, self.strategy_space, self.strategy_space, self.n_cards), dtype=np.int8)
-        self.vs_summary = np.zeros((self.strategy_space, self.strategy_space), dtype=np.float)
-        self.summary = np.zeros(self.strategy_space, dtype=np.float)
+        self.scores = np.zeros((self.n_strategies, self.n_strategies, self.n_deals, self.n_cards), dtype=np.int8)
+        self.vs_summary = np.zeros((self.n_strategies, self.n_strategies), dtype=np.float)
+        self.summary = np.zeros(self.n_strategies, dtype=np.float)
 
     def compute_strategy_outcomes(self):
         deals, cards = self.dealer_space.shape
-        for n in range(1, deals):
+        for n in range(1, self.n_strategies):
             # Roll each strategy by n and compare it to the original to see which would win for what card
             # For a roll of 1 with 4 strategies [s1, s2, s3, s4] vs [s2, s3, s4, s1]
-            p1_beats_p2 = self.dealer_space > np.roll(self.dealer_space, n, axis=0)
-            p2_beats_p1 = self.dealer_space < np.roll(self.dealer_space, n, axis=0)
+            p1_beats_p2 = (self.dealer_space[self.strategies, :] > np.roll(self.dealer_space[self.strategies, :], n, axis=0))
+            p2_beats_p1 = (self.dealer_space[self.strategies, :] < np.roll(self.dealer_space[self.strategies, :], n, axis=0))
             # Record the outcomes such that outcome[deal_id, card] = 1: 1: s1 wins, 0: draw, -1: s2 wins
             outcomes = 1 * p1_beats_p2 - 1 * p2_beats_p1
             # For each card value convert the outcomes into the correct space, eg np.diag for n=1 and 4 strategies at an arbitraty card is
@@ -59,6 +66,10 @@ class Game:
     def compute_summary_scores(self):
         self.vs_summary += np.sum(np.sum(self.scores, axis=3) > 0, axis=2) * 1.0
         self.vs_summary += np.sum(np.sum(self.scores, axis=3) == 0, axis=2) * 0.5
-        self.vs_summary /= self.strategy_space
+        self.vs_summary /= self.n_deals
         self.summary = np.sum(self.vs_summary, axis=1)
-        self.summary *= 1 / self.strategy_space
+        self.summary *= 1 / self.n_strategies
+
+    def compute_hamming_distances(self):
+        return np.sum(np.abs(self.dealer_space - self.dealer_space[0, :][np.newaxis, :]), axis=1)
+
